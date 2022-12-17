d49b4871cb178681f6e6888f7d6f2a1d8b13f0cd
Pat Holland <pholls@users.noreply.github.com>
2022-12-17 15:09:18 -0800

    adding a little commenting

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  # could have made this polymorphic with Answer, but went with separate models instead
   has_many :answers, dependent: :destroy
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include ActiveModel::SecurePassword
 
+  # could have used devise here, went with lower-weight bcrypt
   has_secure_password
 
   validates :name, :email, presence: true, uniqueness: true
