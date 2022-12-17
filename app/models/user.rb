6786c8043cc1dc3aa8b467eff354c68ae06975be
Pat Holland <pholls@users.noreply.github.com>
2022-12-17 15:03:01 -0800

    customize user scaffolding

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :name, :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
   attr_accessor :password_digest
   has_many :questions
   has_many :answers
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create, :show]
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/sessions", to: "sessions#destroy"
+
   resources :questions
   resources :answers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
