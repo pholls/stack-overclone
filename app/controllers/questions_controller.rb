dcc6199da9d82680e95e7291e6e793dc4fb30723
Pat Holland <pholls@users.noreply.github.com>
2022-12-17 15:01:38 -0800

    add empty questions actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,24 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
+
