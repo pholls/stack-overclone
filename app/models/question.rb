8ef2a33da6613def87e3552dfc041cfca44191b1
Pat Holland <pholls@users.noreply.github.com>
2022-12-18 13:43:00 -0800

    add scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,21 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :body, :user_id, :title, presence: true
+
+  scope :unanswered, -> { where.missing(:answers) }
+  scope :answered, -> { left_outer_joins(:answers) }
+  scope :accepted, -> { answered.where( answers: { accepted: true } ) }
+  scope :unaccepted, -> { answered.where( answers: { accepted: false } ).uniq }
+
+  def accepted?
+    answers.accepted.any?
+  end
+
+  def accepted_answer
+    answers.accepted.first
+  end
+
+  def unaccepted_answers
+    answers.unaccepted
+  end
 end
