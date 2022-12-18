40953b9e1d8384cf199c2a59ba0995c7d3ba3f21
Pat Holland <pholls@users.noreply.github.com>
2022-12-18 13:43:11 -0800

    create, accept, unaccept

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,28 @@
 class AnswersController < ApplicationController
   def index
+    # get all answers for current user
   end
 
   def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params.merge(user_id: current_user.id))
+    if @answer.valid?
+      redirect_to @answer.question
+    else
+      redirect_to @answer.question, notice: 'error'
+    end
+  end
+
+  def accept
+    @answer = Answer.find(params[:id])
+    @answer.accept!
+    redirect_to @answer.question
+  end
+
+  def unaccept
+    @answer = Answer.find(params[:id])
+    @answer.unaccept!
+    redirect_to @answer.question
   end
 
   def new
@@ -18,5 +38,11 @@ class AnswersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
