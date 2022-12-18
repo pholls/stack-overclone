9ad4b649b6d0d4f9d335191a02cbd675b04eaa7e
Pat Holland <pholls@users.noreply.github.com>
2022-12-18 13:43:27 -0800

    create, show, new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,21 +4,35 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.create(question_params.merge(user_id: current_user.id))
+    if @question.valid?
+      redirect_to @question
+    else
+      render new_question_path, notice: 'error'
+    end
   end
 
   def new
+    @question = Question.new
   end
 
   def edit
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
 
